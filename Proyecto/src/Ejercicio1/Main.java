2b672632845f9bedf7cd07e43f3d3835a9a2b91f
JoseMiguelPomares <josedeberes2709@gmail.com>
2024-09-23 16:53:21 +0200

    Ejercicio 1

--- Proyecto/src/Ejercicio1/Main.java
+++ Proyecto/src/Ejercicio1/Main.java
@@ -15,17 +15,33 @@ public class Main {
         n1 = scanner.nextInt();
         System.out.println("Escribe el n2: " );
         n2 = scanner.nextInt();
-        numeros(n1, n2);
+        try{
+            numeros(n1, n2);
+        } catch (InterruptedException ie){
+            System.out.println("Hilo interrumpido");
+        }
+
     }
 
-    public static void numeros(int n1, int n2){
+    public static void numeros(int n1, int n2) throws InterruptedException{
         System.out.println("El hilo se ha lanzado");
 
         if (n1<n2){
-            while (n1 < n2){
+            while (n1 <= n2){
+                System.out.println(n1);
                 n1++;
-
+                Thread.sleep(1000);
             }
+        }
+        else if(n1 > n2){
+            while (n1 >= n2){
+                System.out.println(n2);
+                n2++;
+                Thread.sleep(1000);
+            }
+        }
+        else {
+            System.out.println(n1);
         }
     }
 }
