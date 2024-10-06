d65c5cb06d179c06891b3e19990ddf6ce838c8da
VarusNashor <josedeberes2709@gmail.com>
2024-10-06 17:22:51 +0200

    6/10

--- Proyecto/src/Ejercicio2/Ejercicio2Hilo.java
+++ Proyecto/src/Ejercicio2/Ejercicio2Hilo.java
@@ -6,8 +6,31 @@ import java.util.Scanner;
 public class Ejercicio2Hilo implements Runnable{
     @Override
     public void run(){
+        Thread t = Thread.currentThread();
         Random r = new Random();
-        int n;
-        n = r.nextInt(1, 100);;
+        int n, j = 2;
+        boolean primo = true;
+        n = r.nextInt(1, 100);
+        System.out.println("Mostrando primos hasta el " + n);
+
+        for (int i = 3; i < n; i++){
+            while (j < i){
+                if (i % j == 0){
+                    primo = false;
+                    j=i;
+                }
+                j++;
+            }
+            if (primo){
+                System.out.println(t.getName() + " " + i);
+            }
+            try {
+                Thread.sleep(r.nextInt(500, 1000));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            primo = true;
+            j = 2;
+        }
     }
 }
