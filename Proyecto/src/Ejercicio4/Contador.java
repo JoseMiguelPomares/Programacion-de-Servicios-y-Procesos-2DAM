6404a3bae2a865a19866cb9be3d3a69bd7882cd9
VarusNashor <josedeberes2709@gmail.com>
2024-10-11 17:30:44 +0200

    Ejercicio 4

--- Proyecto/src/Ejercicio4/Contador.java
+++ Proyecto/src/Ejercicio4/Contador.java
@@ -4,36 +4,28 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Contador implements Runnable {
+    Random random = new Random();
+    public boolean ejecutar = true;
+    public int num = random.nextInt(10, 20);
+    public int contador = 0;
+
+    public void Detener() { ejecutar = false; }
+    public int getNum() { return num; }
+    public int getContador() { return contador; }
+
     @Override
     public void run(){
-        Thread thread = Thread.currentThread();
-        Scanner myScan = new Scanner(System.in);
-        Random random = new Random();
-        int num = random.nextInt(10, 20);
-
         System.out.println("Pulsar enter cuando creas que el contador ha llegado a " + num);
-        for (int i = 1; i <= num + 1; i++){
+        while (ejecutar) {
+            contador++;
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            if (i <= 5){
-                System.out.println(i);
-            }
-            if (i == num && myScan.nextLine() == "") {
-                System.out.println("Lo has conseguido");
-                thread.stop();
-            }
-            else if (i < num && myScan.nextLine() == "") {
-                System.out.println("Vuelve ha intentarlo, has detenido el contador en " + i);
-                thread.stop();
-            }
-            else if (i > num) {
-                System.out.println("Se ha pasado el contador");
-                thread.stop();
+            if (contador <= 5){
+                System.out.println(contador);
             }
         }
-
     }
 }
--- Proyecto/src/Ejercicio4/Main.java
+++ Proyecto/src/Ejercicio4/Main.java
@@ -5,8 +5,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[]args){
         Scanner myScan = new Scanner(System.in);
-        Thread thread = new Thread(new Contador());
+        Contador contador = new Contador();
+        Thread thread = new Thread(contador);
 
         thread.start();
+
+        myScan.nextLine();
+
+        contador.Detener();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (contador.getContador() == contador.getNum()) {
+            System.out.println("Lo has conseguido");
+        }
+        else if (contador.getContador() < contador.getNum()) {
+            System.out.println("Vuelve ha intentarlo, has detenido el contador en " + contador.getContador());
+        }
+        else if (contador.getContador() > contador.getNum()) {
+            System.out.println("Se ha pasado el contador");
+
+        }
     }
 }
