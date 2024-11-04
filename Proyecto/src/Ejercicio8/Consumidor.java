2b0fb0ec3fb68b82047fe00a81b2bf9c1bb6dbca
JoseMiguelPomares <josedeberes2709@gmail.com>
2024-11-04 16:55:58 +0100

    4/11

--- Proyecto/src/Ejercicio8/Consumidor.java
+++ Proyecto/src/Ejercicio8/Consumidor.java
@@ -4,23 +4,21 @@ public class Consumidor implements Runnable{
 
     @Override
     public void run() {
-        System.out.println("HOLA");
-        synchronized (ProducirCapsulas.contenedor){
-            System.out.println("HOLA 2");
-            System.out.println(ProducirCapsulas.contenedor.size());
-            while (ProducirCapsulas.contenedor.size() >= 6) {
-                System.out.println("Creando caja con 6 capsulas");
-                for (int i = 0; i < 6; i++) {
-                    ProducirCapsulas.contenedor.remove(i);
+        while (true){
+            synchronized (ProducirCapsulas.contenedor){
+                while (ProducirCapsulas.contenedor.size() >= 6) {
+                    System.out.println("Creando caja con 6 capsulas");
+                    for (int i = 0; i < 6; i++) {
+                        ProducirCapsulas.contenedor.remove(0);
+                    }
+                    System.out.println("Caja creada");
                 }
-                System.out.println("Caja creada");
-            }
-            while (ProducirCapsulas.contenedor.size() < 6) {
-                try {
-                    System.out.println("Esperando a que haya 6 capsulas");
-                    ProducirCapsulas.contenedor.wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                while (ProducirCapsulas.contenedor.size() < 6) {
+                    try {
+                        ProducirCapsulas.contenedor.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
         }
