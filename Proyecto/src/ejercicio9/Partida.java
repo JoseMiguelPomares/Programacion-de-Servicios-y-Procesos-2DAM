97c92424788a2da8513df07ffa22ab455ba96182
JoseMiguelPomares <josedeberes2709@gmail.com>
2024-11-27 19:53:22 +0100

    1

--- Proyecto/src/ejercicio9/Partida.java
+++ Proyecto/src/ejercicio9/Partida.java
@@ -1,6 +1,7 @@
 package ejercicio9;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Partida implements Runnable {
@@ -15,20 +16,26 @@ public class Partida implements Runnable {
         boolean partidaGanada = false;
         Random r = new Random();
         int randomNum;
-        ArrayList<Integer> random = new ArrayList<>();
+
+        //System.out.println("hola");
         while (!partidaGanada){
+            ArrayList<Integer> random = new ArrayList<>();
+            //System.out.println("hola2");
             for (int i = 0; i < dados; i++) {
-                try{
-                    Thread.sleep(100);
-                }
-                catch(InterruptedException e){
-                    e.printStackTrace();
-                }
                 randomNum = r.nextInt(1,6);
                 random.add(randomNum);
             }
+            //System.out.println("RESULTADO DADOS " + random.toString());
+            try{
+                Thread.sleep(100);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+            //System.out.println("hola4");
             if (resultado.equals(random)){
-                System.out.println("Partida Ganada!");
+                System.out.println("Partida Ganada! " + resultado.toString());
                 partidaGanada = true;
             }
         }
